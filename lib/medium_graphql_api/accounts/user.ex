d86573449bdfdbf753e79f4de735856abe90a4ed
Maartz <maartz@protonmail.com>
2019-10-12 22:29:39 +0200

    Implements user model validation

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -3,11 +3,13 @@ defmodule MediumGraphqlApi.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field(:email, :string)
+    field(:email, :string, unique: true)
     field(:first_name, :string)
     field(:last_name, :string)
     field(:password_hash, :string)
-    field(:role, :string)
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+    field(:role, :string, default: "user")
 
     timestamps()
   end
@@ -15,7 +17,37 @@ defmodule MediumGraphqlApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :email, :password_hash, :role])
-    |> validate_required([:first_name, :last_name, :email, :password_hash, :role])
+    |> cast(attrs, [
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :role
+    ])
+    |> validate_required([
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :role
+    ])
+    # Validate the email
+    |> validate_format(:email, ~r/@/)
+    # Put the email to downcase
+    |> update_change(:email, &String.downcase(&1))
+    # Validate length min and max
+    |> validate_length(:password, min: 8, max: 100)
+    # Validate password and password_confirmation
+    |> validate_confirmation(:password)
+    # Unique email
+    |> unique_constraint(:email)
+    # Hash the password
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
